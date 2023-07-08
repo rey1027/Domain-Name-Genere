2d7172e774208c2db2c242bbe162cf6836dbaf49
Rachel Pereira <62966013+rey1027@users.noreply.github.com>
2023-07-08 02:49:47 +0000

    Agregar el codigo d al archivo de src

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
-let pronoun = ['the','our','I','she'];
-let adj = ['great', 'big','fun','tall' ];
-let noun = ['jogger','racoon','turle','dog'];
-let verb = ['run','look','write','ride'];
+let pronoun = ['the','our','I','she','he'];
+let adj = ['great', 'big','fun','tall','elegant' ];
+let noun = ['jogger','racoon','turle','dog','rabbit'];
+let verb = ['run','look','write','ride','listen'];
 
 let counter=0;
 
-for(let p = 0; p > pronoun.length; p++){
-    for(let a = 0; a > adj.length; a++){
-        for(let n = 0; n > noun.length; n++){
-            for(let v = 0; v > verb.length; v++){
+for(let p = 0; p < pronoun.length; p++){
+    for(let a = 0; a < adj.length; a++){
+        for(let n = 0; n < noun.length; n++){
+            for(let v = 0; v< verb.length; v++){
                 console.log(pronoun[p]+adj[a]+noun[n]+verb[v]+".com");
                 counter++;
 
@@ -20,3 +20,4 @@ for(let p = 0; p > pronoun.length; p++){
     }
   
 }
+console.log("Combinaciones: ",counter);
